ab70f933615d5d8225449e8b79567e493586c724
Johann <jvillalv@iu.edu>
2021-07-27 21:46:00 -0400

    ready for heroku deployment

---
+++ src/app.py
@@ -0,0 +1,11 @@
+import flask
+from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def landing():
+
+    return render_template("index.html")
+
